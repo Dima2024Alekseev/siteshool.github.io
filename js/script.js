f03ba078ef427195ce7633d4aac3b3aad538b626
Дмитрий <158749849+Dima2024Alekseev@users.noreply.github.com>
2024-02-23 15:56:34 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,11 +10,25 @@ document.body.onload = function(){
 }
 
 
-
-const drop = noselect
-function noselect() {
-    return false
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const backToTop = document.getElementById("back-to-top");
+    
+    // Показать кнопку при загрузке страницы
+    backToTop.style.display = "block";
+    
+    window.addEventListener("scroll", function () {
+        if (window.pageYOffset > 300) {
+            backToTop.style.display = "block";
+        } else {
+            backToTop.style.display = "none";
+        }
+    });
+    
+    backToTop.addEventListener("click", function (event) {
+        event.preventDefault();
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
 
 
 
