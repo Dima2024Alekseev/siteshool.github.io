a08341946c41a4276d228f48610b498abec798be
Дмитрий <158749849+Dima2024Alekseev@users.noreply.github.com>
2024-02-24 19:44:39 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-// document.body.onload = function(){
-//   setTimeout(function(){
-//       var preloader = document.getElementById('page-preloader');
-//       if(!preloader.classList.contains('done'))
-//       {
-//           preloader.classList.add('done');
-//       }
-//   }, 2000);
+document.body.onload = function(){
+  setTimeout(function(){
+      var preloader = document.getElementById('page-preloader');
+      if(!preloader.classList.contains('done'))
+      {
+          preloader.classList.add('done');
+      }
+  }, 2000);
 
-// }
+}
 
 
 
