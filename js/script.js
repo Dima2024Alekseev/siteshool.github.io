98decdda4506315322d5c4e10ecedb05ecf4adde
Дмитрий <158749849+Dima2024Alekseev@users.noreply.github.com>
2024-02-24 19:42:56 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
-document.body.onload = function(){
-  setTimeout(function(){
-      var preloader = document.getElementById('page-preloader');
-      if(!preloader.classList.contains('done'))
-      {
-          preloader.classList.add('done');
-      }
-  }, 2000);
+// document.body.onload = function(){
+//   setTimeout(function(){
+//       var preloader = document.getElementById('page-preloader');
+//       if(!preloader.classList.contains('done'))
+//       {
+//           preloader.classList.add('done');
+//       }
+//   }, 2000);
 
+// }
+
+
+
+const drop = noselect
+function noselect() {
+    return false
 }
+
+
+setInterval(() => {
+    if (document.ondragstart !== drop) {
+        //console.log("Копирование запрещено")
+        document.ondragstart = noselect
+        document.onselectstart = noselect
+        document.oncontextmenu = noselect
+    }
+    else {
+       // console.log('Равны')
+    }
+},
+    10000
+)
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -33,17 +55,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-setInterval(() => {
-    if (document.ondragstart !== drop) {
-        //console.log("Копирование запрещено")
-        document.ondragstart = noselect
-        document.onselectstart = noselect
-        document.oncontextmenu = noselect
-    }
-    else {
-       // console.log('Равны')
-    }
-},
-    10000
-)
+
 
